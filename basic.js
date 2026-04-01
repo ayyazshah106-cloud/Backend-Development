9e043d97047c6d8f58fda100cde5fb549574764c
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-04-01 12:53:36 +0500

    JavaScript Basics

--- basic.js
+++ basic.js
@@ -1,3 +1,52 @@
- var a = 20;
- let b =10;
- console.log(a+b);
+//  // <===================Variables=====================>
+//  var a = 20; //Function scope
+//  let b =10;  //Block scope 
+//  const c = 30;
+//  console.log(a+b+c);
+//  b= 100;
+//  a = 100;
+// //  c= 10;   //Error
+// console.log(a+b+c);
+// // <===================conditions=====================>
+// if(a==100){
+//   console.log('this is if condition');
+// }
+// else{
+//   console.log('this is else condition');
+// }
+
+// function fruits(item){
+//   console.log('This is '+item);
+// }
+// console.log('banana')
+
+// <===================Loops=====================>
+
+// for(var a=0;a<=10;a++){
+//   console.log(a);
+// }
+
+// var a = 0;
+// while(a<=10){
+//   console.log(a) //First consloe then increament output 1 to 10
+//   a++;
+//   // console.log(a) //First increament then consloe output 1 to 11
+// }
+// <===================Array=====================>
+// var user = ["ayyaz","nawaz","mateen"];
+// console.log(user[1]);
+
+
+// var user = ["ayyaz","nawaz","mateen"];
+// // console.log(user[1])
+
+// for(var a=0;a<user.length;a++){
+//   console.log(user[a]);
+// }
+
+// <===================Objects=====================>
+var user = {
+  name :"ayyaz",
+  age: 27
+}
+console.log(user.age);
