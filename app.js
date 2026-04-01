9fabfd7ecfcc5dd56df69e946511e84b39dd6e17
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-04-01 11:34:42 +0500

    file system and javaScript basics

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log(20+20)
+// console.log("Step by Step")
+
+
+// function fruit(item){
+// console.log("Fruit name is "+item);
+// }
+// fruit("apple");
+
+// var a=20;
+// var b=20;
+// console.log(a+b);
+
+
+//  <=====================create file with node js===================>
+// var fs = require('fs');
+// fs.writeFileSync('Create_File.txt','Create file with node js');l
+// // file name.type and file data "require import the modules"
+// repal terminal par code run karna
+
+
