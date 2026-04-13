8dea0d89883ce95190bf6054b689bb732382d284
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-04-13 23:31:33 +0500

    create server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-// create Server
+// create basic Server
 const http = require('http');
 http.createServer((req,resp)=>{
+  resp.write('<h1>this is server</h1>');
 resp.end("hello");
 }).listen(4800);
