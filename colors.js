dcd90e032972a00b53008b8832f57210fa99dad1
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-04-13 23:52:53 +0500

    external pakages installation and make pakage json fils

---
+++ colors.js
@@ -0,0 +1,6 @@
+const colors = require('colors');
+console.log("This is colors pakage");
+console.log(colors.red("this is red color"));
+console.log(colors.green("this is green color"));
+console.log(colors.blue("this is blue color"));
+console.log(colors.white("this is white color"));
