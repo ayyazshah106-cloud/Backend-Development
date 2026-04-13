6a955d7f12074d1043489728729f97c578888b11
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-04-13 22:55:22 +0500

    core modules and global objects

--- modules.js
+++ modules.js
@@ -1,11 +1,24 @@
 // core modules
 
-const fs = require("fs");
-const os = require('os');
-fs.writeFileSync("dummy.txt","learning core modules")
+// const fs = require("fs");
+// const os = require('os');
+// fs.writeFileSync("dummy.txt","learning core modules")
 
 
-console.log(os.platform());
-console.log(os.hostname());
-console.log(os.cpus());
 
+
+// console.log(os.platform());
+// console.log(os.hostname());
+// console.log(os.cpus());
+
+// global Objects
+
+// console.log("abc");
+// console.log(process.cwd());
+
+
+// make forcely into custom objects
+const {log,warn}= require("console");
+log("custom log");
+warn("custom log");
+console.warn(process.pid);
