14bc0a3f5a8f2fb11d931593e21266be3cf22e99
ayyazshah106-cloud <ayyazshah106@gmail.com>
2026-06-14 12:43:01 +0500

    ******* Display data from  Mongodb  on UI with Node js  db with node js**********

--- dynamic_routes.js
+++ dynamic_routes.js
@@ -1,55 +1,55 @@
-import express from "express";
+// import express from "express";
 
-// Create express app
-const app = express();
-
-
-// Root Route
-app.get("/", (req, resp) => {
-
-  // Array of users
-  const users = ["ayyaz", "nawaz", "Aqsa","mohsin","mateen"];
-
-  // Start HTML unordered list
-  let data = `<ul>`;
-
-  // Loop through users array
-  for (let i = 0; i < users.length; i++) {
-
-    // Create dynamic user links
-    data += `
-      <li>
-        <a href="/user/${users[i]}">
-          ${users[i]}
-        </a>
-      </li>
-    `;
-  }
-
-  // Close unordered list
-  data += `</ul>`;
-
-  // Send HTML response
-  resp.send(data);
-});
+// // Create express app
+// const app = express();
 
 
-// Dynamic User Route
-app.get("/user/:name", (req, resp) => {
+// // Root Route
+// app.get("/", (req, resp) => {
 
-  // Get username from URL params
-  const userName = req.params.name;
-  // userName.toUpperCase();
+//   // Array of users
+//   const users = ["ayyaz", "nawaz", "Aqsa","mohsin","mateen"];
+
+//   // Start HTML unordered list
+//   let data = `<ul>`;
+
+//   // Loop through users array
+//   for (let i = 0; i < users.length; i++) {
+
+//     // Create dynamic user links
+//     data += `
+//       <li>
+//         <a href="/user/${users[i]}">
+//           ${users[i]}
+//         </a>
+//       </li>
+//     `;
+//   }
+
+//   // Close unordered list
+//   data += `</ul>`;
+
+//   // Send HTML response
+//   resp.send(data);
+// });
+
+
+// // Dynamic User Route
+// app.get("/user/:name", (req, resp) => {
+
+//   // Get username from URL params
+//   const userName = req.params.name;
+//   // userName.toUpperCase();
   
 
-  // Send dynamic response
-  resp.send(`This is ${userName} page`);
-});
+//   // Send dynamic response
+//   resp.send(`This is ${userName} page`);
+// });
 
 
-// Start server on port 3200
-app.listen(3200, () => {
+// // Start server on port 3200
+// app.listen(3200, () => {
 
-  console.log("Server running on port 3200");
+//   console.log("Server running on port 3200");
 
-});
+// });
---
+++ mongodb2.js
@@ -0,0 +1,21 @@
+import express from "express"
+import { MongoClient } from "mongodb"
+
+const dbName = "school";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+
+
+const app = express();
+app.set("view engine","ejs")
+app.get("/",async (req,resp)=>{
+await client.connect();
+const db = client.db(dbName);
+const collections = db.collection("students");
+const result = await collections.find().toArray();
+console.log(result);
+
+resp.render("student",{result})
+})
+app.listen(3200)
